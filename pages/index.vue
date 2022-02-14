b1ec33cff0a1e9d04b312772043e385a9935bfb2
AyumuNekozuki <ayumu@nekozuki.me>
2022-02-14 18:15:38 +0900

    いったんもどす

--- pages/index.vue
+++ pages/index.vue
@@ -4,24 +4,24 @@
       <!-- todo -->
     </div>
     <div class="news">
-      <nuxt-link :to="'/article/'+article.id" v-for="article in articledata.contents" :key="article">
+      <!-- <nuxt-link :to="'/article/'+article.id" v-for="article in articledata.contents" :key="article">
         <span>{{ $dateFns.format(new Date(article.updatedAt), 'yyyy/MM/dd') }}</span>
         <p>{{ article.title }}</p>
-      </nuxt-link>
+      </nuxt-link> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $axios }){
-    let [articledata] = await Promise.all([
-      $axios.$get(`/api_microcms/v1/article?filters=category%5Bequals%5Dcropura&orders=updatedAt`)
-    ]);
+  // async asyncData({ $axios }){
+  //   let [articledata] = await Promise.all([
+  //     $axios.$get(`/api_microcms/v1/article?filters=category%5Bequals%5Dcropura&orders=updatedAt`)
+  //   ]);
 
-    return {
-      articledata
-    }
-  }
+  //   return {
+  //     articledata
+  //   }
+  // }
 }
 </script>
